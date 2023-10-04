71719855cbb115ba061ffc806990904ea0bb8375
Moowin <rlaalsdn4036@gmail.com>
2023-10-04 16:20:51 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button, TextField, Typography, Container } from '@mui/material';
+import Button2 from 'react-bootstrap/Button';
+function App() {
+  return (
+    <Container>
+      <Typography variant="h2">Hello, Material-UI</Typography>
+      <TextField label="Name" />
+      <Button variant="contained" color="primary">
+        Click me material
+      </Button>
+      <Button2>
+        Click me bootstrap
+      </Button2>
+    </Container>
+  );
+}
+
+export default App;
